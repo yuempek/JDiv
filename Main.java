e21f50489b0ff974c3c564b2a2b7e88908030738
yuempek <yuempek@mynet.com>
2019-02-07 17:11:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,64 +6,76 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
         
-        int width = 500;
-        int defaultHeight = 35;
-        int defaultButtonWidth = 150;
-        
+        int appWidth = 500;
+        int appHeight = 500;
+
         GridLayout layout = new GridLayout();
         
         Div root = new Div(
                        new Div(
-                           new Div(layout)
-                               .floating(Floating.LEFT)
-                               .addComponent(new JLabel("IP"))
-                               .width(40).height(defaultHeight)
-                               .padding(5)
+                           new Div()
+                               .addComponent(new JButton("Test Button"))
+                               .padding(5).border(5).margin(5)
                            ,
-                           new Div(layout)
+                           new Div()
+                               .addComponent("JLabel text")
+                               .padding(5, 20).border(5, 10, 15, 20).margin(5)
+                               .width(200).height(200)
+                           ,
+                           new Div()
+                               .floating(Floating.LEFT)
+                               .width(100).height(400)
+                               .padding(5).border(5).margin(5)
+                           ,
+                           new Div()
+                               .floating(Floating.RIGHT)
+                               .width(200).height(200)
+                               .padding(5).border(5).margin(5)
+                       ).padding(5, 20)
+                       ,
+                       new Div(
+                           new Div()
+                               .floating(Floating.LEFT)
+                               .addComponent("IP")
+                               .width(40).height(defaultHeight)
+                           ,
+                           new Div()
                                .floating(Floating.LEFT)
                                .addComponent(new JTextField("localhost"))
                                .width(200).height(defaultHeight)
-                               .padding(5)
                            ,
-                           new Div(layout)
+                           new Div()
                                .floating(Floating.LEFT)
                                .addComponent(new JButton("Start Connection"))
                                .width(defaultButtonWidth).height(defaultHeight)
-                               .padding(5)
-                       ).padding(0, 20)
+                       )
                        ,
-                       new Div(
-                           new Div(layout)
+                       new Div(new FlowLayout(),
+                           new Div()
                                .floating(Floating.LEFT)
-                               .addComponent(new JLabel("Port"))
+                               .addComponent("Port")
                                .width(40).height(defaultHeight)
-                               .padding(5)
                            ,
-                           new Div(layout)
+                           new Div()
                                .floating(Floating.LEFT)
                                .addComponent(new JTextField("1234"))
                                .width(200).height(defaultHeight)
-                               .padding(5)
                            ,
-                           new Div(layout)
+                           new Div()
                                .floating(Floating.LEFT)
                                .addComponent(new JButton("Stop Connection"))
                                .width(defaultButtonWidth).height(defaultHeight)
-                               .padding(5)
                        )
                    );
         
-        DivRenderer renderer = new DivRenderer(root);
-        
         JFrame frame = new JFrame("JDiv Application"); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+        frame.getContentPane().setLayout(new DivLayout());
         
         JPanel panel = renderer.render();
-        frame.add(panel);
+        frame.add(root);
         
-        frame.setSize(500, 100);
+        frame.setSize(appWidth, appHeight);
         frame.setVisible(true);
         
     }
